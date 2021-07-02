4e65122f27d0e55bd75882b2947bd62f5f09cf90
amongiove <amandamongiove@gmail.com>
2021-07-02 13:10:28 -0500

    commit tree d70b00caa2ea20cbaf413c56eee4b45ebe31e9fd

--- index.js
+++ index.js
@@ -1,3 +1,29 @@
 class Formatter {
   //add static methods here
+  static capitalize(str){
+    return str.charAt(0).toUpperCase() + str.slice(1);
+  }
+
+  static sanitize(str){
+    return str.replace(/[^A-Za-z0-9-'\s]+/g, '')
+  }
+
+  
+
+  static titleize(str){
+    const noCapWords = ["the", 'a', "an", "but", "of", "and", "for", "at", "by", "from"];
+    let words = str.split(' ')
+    let newString = []
+    for (let i = 0; i < words.length; i++)
+    {
+      if (i === 0 || !noCapWords.includes(words[i])){
+        newString.push(this.capitalize(words[i]));
+      }
+      else {
+        newString.push(words[i]);
+      }
+    }
+    return newString.join(' ');
+  }
+
 }
